94b5a870455d35b58650d77762d6b1598ce81054
budiandree <benedictandree@gmail.com>
2019-07-03 20:21:50 +0700

    add exercise 7

---
+++ exercise-7.js
@@ -0,0 +1,30 @@
+// Tugas nomor 1
+var rows1 = 5;
+while(rows1 > 0) {
+    console.log('*');
+    rows1--;
+}
+// Tugas nomor 2
+var rows = 5;
+while (rows > 0) {
+  var cetak = '';
+  var rows1 = 5;
+    while (rows1 > 0) { 
+        var cetak = cetak + '*';
+        rows1 --;
+    } 
+    console.log (cetak);
+    rows --;
+} 
+console.log (cetak);
+
+// Tugas nomor 3
+var rows3 = 5;
+var temp = '';
+while (rows3 > 0) {
+    
+    var temp = temp + '*';
+    rows3 --;
+  
+  console.log(temp)
+}
