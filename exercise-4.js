bb4896b7ff6f2c130a99a0aad8eca3c8e8a7e17d
budiandree <benedictandree@gmail.com>
2019-07-03 15:34:54 +0700

    finishing exercise 4

--- exercise-4.js
+++ exercise-4.js
@@ -1,32 +1,36 @@
-var tanggal = 1; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var tanggal = 20; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
 var bulan = 1; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 2000; // 
 
-switch (bulan) {
-    case 1:
-        console.log(tanggal + ' Januari ' + tahun);
-    case 2:
-        console.log(tanggal + ' Februari ' + tahun);
-    case 3:
-        console.log(tanggal + ' Maret ' + tahun);
-    case 4:
-        console.log(tanggal + ' April ' + tahun);
-    case 5:
-        console.log(tanggal + ' Mei ' + tahun);
-    case 6:
-        console.log(tanggal + ' Juni ' + tahun);
-    case 7:
-        console.log(tanggal + ' Juli ' + tahun);
-    case 8:
-        console.log(tanggal + ' Agustus ' + tahun);
-    case 9:
-        console.log(tanggal + ' September ' + tahun);
-    case 10:
-        console.log(tanggal + ' Oktober ' + tahun);
-    case 11:
-        console.log(tanggal + ' November ' + tahun);
-    case 12:
-        console.log(tanggal + ' Desember ' + tahun);
-    default:
-        console.log('Mohon mengisi bulan antara 1 - 12');
-}
+if (tanggal > 0 && tanggal < 32 && tahun > 1899 && tahun < 2201) {
+    switch (bulan) {
+        case 1:
+            console.log(tanggal + ' Januari ' + tahun); break;
+        case 2:
+            console.log(tanggal + ' Februari ' + tahun); break;
+        case 3:
+            console.log(tanggal + ' Maret ' + tahun); break;
+        case 4:
+            console.log(tanggal + ' April ' + tahun); break;
+        case 5:
+            console.log(tanggal + ' Mei ' + tahun); break;
+        case 6:
+            console.log(tanggal + ' Juni ' + tahun); break;
+        case 7:
+            console.log(tanggal + ' Juli ' + tahun); break;
+        case 8:
+            console.log(tanggal + ' Agustus ' + tahun); break;
+        case 9:
+            console.log(tanggal + ' September ' + tahun); break;
+        case 10:
+            console.log(tanggal + ' Oktober ' + tahun); break;
+        case 11:
+            console.log(tanggal + ' November ' + tahun); break;
+        case 12:
+            console.log(tanggal + ' Desember ' + tahun); break;
+        default:
+            console.log('Mohon mengisi bulan antara 1 - 12'); break;
+     } }
+else {
+    console.log('Mohon mengisi tanggal antara 1 - 31, dan tahun antara 1900 - 2200')
+} 
