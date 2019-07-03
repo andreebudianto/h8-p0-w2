a79d12666a74adac1946c1b3bd3366134bf22066
budiandree <benedictandree@gmail.com>
2019-07-03 15:25:11 +0700

    finisih exercise 4,add exercise 6

--- exercise-4.js
+++ exercise-4.js
@@ -1,7 +1,32 @@
-var word = 'JavaScript';
-var second = 'is';
-var third = 'awesome';
-var fourth = 'and';
-var fifth = 'I';
-var sixth = 'love';
-var seventh = 'it!';
+var tanggal = 1; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 1; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 2000; // 
+
+switch (bulan) {
+    case 1:
+        console.log(tanggal + ' Januari ' + tahun);
+    case 2:
+        console.log(tanggal + ' Februari ' + tahun);
+    case 3:
+        console.log(tanggal + ' Maret ' + tahun);
+    case 4:
+        console.log(tanggal + ' April ' + tahun);
+    case 5:
+        console.log(tanggal + ' Mei ' + tahun);
+    case 6:
+        console.log(tanggal + ' Juni ' + tahun);
+    case 7:
+        console.log(tanggal + ' Juli ' + tahun);
+    case 8:
+        console.log(tanggal + ' Agustus ' + tahun);
+    case 9:
+        console.log(tanggal + ' September ' + tahun);
+    case 10:
+        console.log(tanggal + ' Oktober ' + tahun);
+    case 11:
+        console.log(tanggal + ' November ' + tahun);
+    case 12:
+        console.log(tanggal + ' Desember ' + tahun);
+    default:
+        console.log('Mohon mengisi bulan antara 1 - 12');
+}
---
+++ exercise-6.js
@@ -0,0 +1,39 @@
+// Tugas nomor 1
+console.log('Looping Pertama'.toUpperCase())
+var loop = 2;
+while(loop < 21) { 
+  console.log(loop + ' - I love coding ' ); 
+  loop = loop + 2; 
+}
+
+console.log('Looping Kedua'.toUpperCase())
+var loop1 = 20;
+while(loop1 > 1) { 
+  console.log(loop1 + ' - I will become full stack developer ' ); 
+  loop1 = loop1 - 2; 
+}
+
+// Tugas nomor 2
+console.log('Looping Pertama'.toUpperCase())
+for(var loop3 = 1; loop3 < 21; loop3++) {
+    console.log(loop3 + ' - I love coding');
+}
+
+console.log('Looping Kedua'.toUpperCase())
+for(var loop4 = 20; loop4 > 0; loop4--) {
+    console.log(loop4 + ' - I will become fullstack developer');
+}
+
+//Tugas nomor 3
+var counter = 0
+while(counter < 101) {
+    counter++;
+    if ((counter%2) == 0) {
+        console.log(counter +' adalah Genap');
+    } else {
+        console.log(counter + ' adalah Ganjil');
+    }
+}
+
+
+  
